b0b4a3aa43f8a143d4f5b9dbee65cd408318c85d
unknown <Tarjunior>
2022-02-08 12:59:03 +0100

    suite du projet ecommerce

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -17,39 +17,46 @@ class CategoryController extends AbstractController
     #[Route('/', name: 'category_index', methods: ['GET'])]
     public function index(CategoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findAll();
+
         return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
+            'categories' => $categories,
         ]);
     }
 
     #[Route('/new', name: 'category_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        // On créé une instance  de la classe catégorie
+        //On créé une instance de la classe catégorie
         $category = new Category();
-        
+
         //On créé un formulaire d'un certain type : CategoryType
-        //On y injecte l'instance de la classe catégorie
+        //On y injecte l'instance de la classe catégorie précédemment créée
         $form = $this->createForm(CategoryType::class, $category);
+
+        //Cela permet de traiter les données du formulaire
         $form->handleRequest($request);
 
-        //
-        //
+        //Si le formulaire a été soumis et si les données sont valides
+        //Alors je gère l'ajout de la catégorie en base de données
         if ($form->isSubmitted() && $form->isValid()) {
 
             //L'entity manager persist l'objet catégorie
-            // Mon entity manager prepare la catégorie a aleer en base de données
+            //Mon entity manager, prépare la catégorie a aller en base de données
             $entityManager->persist($category);
 
             //L'entity manager envoie pour de bon les données en base.
             $entityManager->flush();
 
+            //J'envoie un message flash
             $this->addFlash("success","La catégorie a bien été ajouté.");
 
-            // Je 
+            //Je redirige vers la route de mon choix.
             return $this->redirectToRoute('category_index', [], Response::HTTP_SEE_OTHER);
+
         }
-        //Ici, si le formulaire n'as pas été soumis, on affiche la page
+
+        //Ici, si le formulaire n'a pas été soumis, on affiche la page
         return $this->render('category/new.html.twig', [
             'category' => $category,
             'form' => $form->createView(),
@@ -68,6 +75,7 @@ class CategoryController extends AbstractController
     public function edit(Request $request, Category $category, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(CategoryType::class, $category);
+        
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -21,6 +23,14 @@ class Category
     #[ORM\Column(type: 'string', length: 255)]
     #[Assert\NotBlank(message:'Le champs image est requis.')]
     private $image;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class, orphanRemoval: true)]
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -50,4 +60,35 @@ class Category
 
         return $this;
     }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getCategory() === $this) {
+                $product->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
 }
